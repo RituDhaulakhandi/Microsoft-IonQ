421173094a2e6ed2e26be921cc649b44b52c1023
RituDhaulakhandi <75118290+RituDhaulakhandi@users.noreply.github.com>
2022-01-30 20:09:16 +0530

    Update quantum.py

--- quantum.py
+++ quantum.py
@@ -1,3 +1,83 @@
+from qiskit import *
+import unit
+import gui
+import room
+import fonts as f
+from azure.quantum.qiskit import AzureQuantumProvider
+from qiskit.tools.monitor import job_monitor
+from qiskit.visualization import plot_histogram
+from qiskit import QuantumCircuit, Aer, execute
+from qiskit.visualization import plot_histogram
+
+
+resource_id = "/subscriptions/b1d7f7f8-743f-458e-b3a0-3e09734d716d/resourceGroups/aq-hackathons/providers/Microsoft.Quantum/Workspaces/aq-hackathon-01"
+
+provider = AzureQuantumProvider(
+    resource_id=resource_id,
+    location= "East US"
+)
+all_pairs = ['00','01','10','11']
+class Quantum():
+    #backend = provider.get_backend("ionq.simulator")
+    def __init__(self, parent, child, attribute):
+        self.parent: unit.Unit = parent
+        self.child: unit.Unit = child
+        self.attribute: str = attribute
+
+        # prepare the quantum circuit
+        # this is only for entanglement (I think?)
+        q = QuantumRegister(2, "q")  # quantum register with 2 qubits
+        c = ClassicalRegister(2, "c")  # classical register with 2 bits
+        self.qc = QuantumCircuit(q, c)  # quantum circuit with quantum and classical registers
+
+        # prepare the quantum circuit
+        # this is only for entanglement (I think?)
+        self.qc.h(q[0])
+        self.qc.cx(q[0], q[1])
+        self.qc.barrier()
+        self.qc.measure(q, c)
+        # job = backend.run(qc, shots=100)
+        # job_monitor(job)
+
+    def run_job(self):
+        backend = provider.get_backend("ionq.simulator")
+        job = backend.run(self.qc, shots=10)
+        job_monitor(job)
+        return job
+
+    def measure(self):  # I think this needs to get multithreaded to avoid conflict with GUI
+        # return True
+        job = self.run_job()
+        result = job.result()
+        counts = result.get_counts(self.qc)
+        print(result)
+        print(counts)
+        entangled_states = counts.keys()
+        if ('00' in entangled_states and '11' in entangled_states):
+            return True
+        else:
+            return False
+
+
+
+    def observe(self):
+        if self.measure():
+            modal = gui.Dialog(f"SWITCHAROO!!! Switched stat {self.attribute}",
+                              f.MAIN, layout=room.Layout(gravity=gui.Gravity.CENTER), dismiss_callback=True,
+                              clear_screen=None)
+            room.run_room(modal)
+            print(f"SWITCHAROO!!! Switched stat {self.attribute}")
+            tmp = getattr(self.parent, self.attribute)
+            setattr(self.parent, self.attribute, getattr(self.child, self.attribute))
+            setattr(self.child, self.attribute, tmp)
+        
+
+    def edit_circuit(): # allow player to manipulate circuit themselves if there's time
+        pass
+
+'''
+Work in progress: Alternate code for quantum.py file with additional changes
+
 #from qiskit import *
 import unit
 import gui
@@ -95,3 +175,4 @@ class Quantum():
 
     def edit_circuit(): # allow player to manipulate circuit themselves if there's time
         pass
+'''
